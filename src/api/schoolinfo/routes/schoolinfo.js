79b82d8565cc905e949f67d3e14611eae16eaaee
Sabrina Hennrich <sabrina@digimedialoop.de>
2024-04-23 08:46:02 +0200

    init

---
+++ src/api/schoolinfo/routes/schoolinfo.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * schoolinfo router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::schoolinfo.schoolinfo');
---
+++ src/api/schoolinfo/services/schoolinfo.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * schoolinfo service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::schoolinfo.schoolinfo');
